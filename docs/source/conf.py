c7ab490e8e0eebedc9c201c10c7c07f3fcb798db
Andrew Sampson <andrewsampson819@gmail.com>
2015-03-17 01:12:10 -0500

    Add favicon, adjust html short title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -122,7 +122,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'Cities:Skylines Modding Documentation'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -131,7 +131,7 @@ html_theme = 'sphinx_rtd_theme'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = '_static/skylines-favicon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
